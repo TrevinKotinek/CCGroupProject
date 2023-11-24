39f695a7b3535377a66abda81ab596a6e4891fed
Trevin Kotinek <trevinkotinek@gmail.com>
2023-11-24 14:08:23 -0600

    Add routing

---
+++ book-tables/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllBooksComponent } from './all-books/all-books.component';
+import { DescPageComponent } from './desc-page/desc-page.component';
+
+const routes:Routes = [
+    {path:'', redirectTo: '/all-books', pathMatch: 'full'},
+    {path:'all-books', component:AllBooksComponent},
+    {path:'description', component:DescPageComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
+export {routes};
--- book-tables/src/app/app.module.ts
+++ book-tables/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AllBooksComponent } from './all-books/all-books.component';
 import { DescPageComponent } from './desc-page/desc-page.component';
+import { routes } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { DescPageComponent } from './desc-page/desc-page.component';
     DescPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
