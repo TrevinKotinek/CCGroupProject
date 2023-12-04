f240e1bb792420c74b28e3c493d9066368a8ccb7
sammy-uno <mustaphafriha@gmail.com>
2023-12-04 15:33:23 -0600

    new changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/BookStore
 cd book-tables
 npm install
 #ng build
-ng build --source-map=false
+ng build --source-map=false --watch=true
 cd /home/ec2-user
 chown -R "ec2-user:ec2-user" *
 sleep 60
