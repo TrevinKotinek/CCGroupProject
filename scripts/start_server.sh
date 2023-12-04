b4ccaa602110aa1501fb76b571e71d35b757338a
sammy-uno <mustaphafriha@gmail.com>
2023-12-04 14:44:10 -0600

    new changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo chmod 755 /home/ec2-user
 cd /home/ec2-user/BookStore
-sudo systemctl reload nginx
-sudo systemctl restart nginx
+#sudo systemctl reload nginx
+#sudo systemctl restart nginx
+sudo systemctl start nginx
