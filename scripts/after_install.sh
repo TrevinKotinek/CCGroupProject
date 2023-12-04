c5e22265ba6e160d9d5484a652bbea87a52d6f97
sammy-uno <mustaphafriha@gmail.com>
2023-12-04 06:18:36 -0600

    new changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,10 +3,10 @@ cd /home/ec2-user/BookStore
 cd book-tables
 npm install
 ng build
-cd ~
+cd /home/ec2-user
 chown -R "ec2-user:ec2-user" *
 sleep 120
-cp -r BookStore/book-tables/dist ~
+cp -r BookStore/book-tables/dist .
 
 
 
